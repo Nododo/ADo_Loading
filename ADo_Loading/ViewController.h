24757e0380bfe8376ad3af791af9e07756ded2be
DWX <nododo1027@gmail.com>
2015-10-26 10:10:53 +0800

    Initial Commit

---
+++ ADo_Loading/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ADo_Loading
+//
+//  Created by 杜维欣 on 15/10/26.
+//  Copyright © 2015年 杜维欣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ADo_LoadingTests/ADo_LoadingTests.m
@@ -0,0 +1,39 @@
+//
+//  ADo_LoadingTests.m
+//  ADo_LoadingTests
+//
+//  Created by 杜维欣 on 15/10/26.
+//  Copyright © 2015年 杜维欣. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ADo_LoadingTests : XCTestCase
+
+@end
+
+@implementation ADo_LoadingTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
